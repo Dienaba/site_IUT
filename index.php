a0ba7f6b10f5b0f2e4d6fbe4014364c8683e98ec
darealsoapbox <nicolas@wpandco.fr>
2018-10-26 16:49:46 +0200

    index.php modif

--- index.php
+++ index.php
@@ -1,27 +1,5 @@
-<!DOCTYPE HTML5>
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
-        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <header>
-            <?php include('header.php'); ?>
-        </header>
+<?php include('header.php'); ?>
         <main class="container">
             <div>Hello World</div>
         </main>
-        <footer class="container">
-            <div>
-                <ul>
-                    <li>Test</li>
-                    <li>Test</li>
-                    <li>Test</li>
-                    <li>Test</li>
-                    <li>Test</li>
-                </ul>
-            </div>
-        </footer> 
-    </body>
-</html>
+<?php includ ('footer.php'); ?>
