bd4e8d4bcc29842c391a04beb73598dd32b9b033
Nabil <nabilmes.contact@gmail.com>
2018-11-19 01:50:13 +0100

    ajout footer

--- header.php
+++ header.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link href="css/mdb.min.css" rel="stylesheet">
     </head>
 
     <body>
