5cdd43ec04a808b3bbff69029304f74da8d2a1e9
darealsoapbox <nicolas@wpandco.fr>
2018-11-20 14:45:30 +0100

    page formation

--- template-formation.php
+++ template-formation.php
@@ -1,40 +1,42 @@
 <?php include('header.php'); ?>
 
 <main class="container">
-    <div class="structure-du-carre">
-        <div class="titre-de-la-formation">Première formation</div>
-        <div class="lieu-de-la-formation"></div>
-        <div class="description de la formation">
-            Aenean libero erat, accumsan et eleifend vel, rutrum at dolor.
-            Quisque volutpat lacus at nunc rutrum, lacinia eleifend risus consectetur.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ut metus nisl.
-            Morbi ligula ligula, laoreet a rutrum nec, scelerisque vitae nibh. Fusce erat libero, pulvinar ac erat quis, laoreet suscipit est.
-            Pellentesque sit amet justo purus.
-            Suspendisse finibus feugiat venenatis. Praesent gravida a arcu in lobortis. Suspendisse vitae rutrum nulla.
+    <div class="container-des-carres">
+        <div class="structure-du-carre">
+            <div class="titre-de-la-formation">Première formation</div>
+            <div class="lieu-de-la-formation"></div>
+            <div class="description de la formation">
+                Aenean libero erat, accumsan et eleifend vel, rutrum at dolor.
+                Quisque volutpat lacus at nunc rutrum, lacinia eleifend risus consectetur.
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ut metus nisl.
+                Morbi ligula ligula, laoreet a rutrum nec, scelerisque vitae nibh. Fusce erat libero, pulvinar ac erat quis, laoreet suscipit est.
+                Pellentesque sit amet justo purus.
+                Suspendisse finibus feugiat venenatis. Praesent gravida a arcu in lobortis. Suspendisse vitae rutrum nulla.
+            </div>
         </div>
-    </div>
-    <div class="structure-du-carre">
-        <div class="titre-de-la-formation">Première formation</div>
-        <div class="lieu-de-la-formation"></div>
-        <div class="description de la formation">
-            Aenean libero erat, accumsan et eleifend vel, rutrum at dolor.
-            Quisque volutpat lacus at nunc rutrum, lacinia eleifend risus consectetur.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ut metus nisl.
-            Morbi ligula ligula, laoreet a rutrum nec, scelerisque vitae nibh. Fusce erat libero, pulvinar ac erat quis, laoreet suscipit est.
-            Pellentesque sit amet justo purus.
-            Suspendisse finibus feugiat venenatis. Praesent gravida a arcu in lobortis. Suspendisse vitae rutrum nulla.
+        <div class="structure-du-carre">
+            <div class="titre-de-la-formation">Première formation</div>
+            <div class="lieu-de-la-formation"></div>
+            <div class="description de la formation">
+                Aenean libero erat, accumsan et eleifend vel, rutrum at dolor.
+                Quisque volutpat lacus at nunc rutrum, lacinia eleifend risus consectetur.
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ut metus nisl.
+                Morbi ligula ligula, laoreet a rutrum nec, scelerisque vitae nibh. Fusce erat libero, pulvinar ac erat quis, laoreet suscipit est.
+                Pellentesque sit amet justo purus.
+                Suspendisse finibus feugiat venenatis. Praesent gravida a arcu in lobortis. Suspendisse vitae rutrum nulla.
+            </div>
         </div>
-    </div>
-    <div class="structure-du-carre">
-        <div class="titre-de-la-formation">Première formation</div>
-        <div class="lieu-de-la-formation"></div>
-        <div class="description de la formation">
-            Aenean libero erat, accumsan et eleifend vel, rutrum at dolor.
-            Quisque volutpat lacus at nunc rutrum, lacinia eleifend risus consectetur.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ut metus nisl.
-            Morbi ligula ligula, laoreet a rutrum nec, scelerisque vitae nibh. Fusce erat libero, pulvinar ac erat quis, laoreet suscipit est.
-            Pellentesque sit amet justo purus.
-            Suspendisse finibus feugiat venenatis. Praesent gravida a arcu in lobortis. Suspendisse vitae rutrum nulla.
+        <div class="structure-du-carre">
+            <div class="titre-de-la-formation">Première formation</div>
+            <div class="lieu-de-la-formation"></div>
+            <div class="description de la formation">
+                Aenean libero erat, accumsan et eleifend vel, rutrum at dolor.
+                Quisque volutpat lacus at nunc rutrum, lacinia eleifend risus consectetur.
+                Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer ut metus nisl.
+                Morbi ligula ligula, laoreet a rutrum nec, scelerisque vitae nibh. Fusce erat libero, pulvinar ac erat quis, laoreet suscipit est.
+                Pellentesque sit amet justo purus.
+                Suspendisse finibus feugiat venenatis. Praesent gravida a arcu in lobortis. Suspendisse vitae rutrum nulla.
+            </div>
         </div>
     </div>
 </main>
