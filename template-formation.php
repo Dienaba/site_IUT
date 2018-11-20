811d687ed06be020dafade1c7c4163259a6aa25d
darealsoapbox <nicolas@wpandco.fr>
2018-11-20 16:06:23 +0100

    iut de bordeaux

--- template-formation.php
+++ template-formation.php
@@ -20,8 +20,8 @@
             </div>
         </div>
         <div class="structure-du-carre">
-            <h4 class="titre-de-la-formation">Deuxième formation</h4>
-            <div class="lieu-de-la-formation"></div>
+            <h4 class="titre-de-la-formation">IUT de Bordeaux</h4>
+            <div class="lieu-de-la-formation"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1889.788443429126!2d-0.6092638021238668!3d44.79133470283238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd54d8a2d04d5aab%3A0xd64d581ac75e7b8!2sIUT+GEII!5e0!3m2!1sfr!2sfr!4v1542726359226" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe></div>
             <div class="description-de-la-formation">
                 Aenean libero erat, accumsan et eleifend vel, rutrum at dolor.
                 Quisque volutpat lacus at nunc rutrum, lacinia eleifend risus consectetur.
