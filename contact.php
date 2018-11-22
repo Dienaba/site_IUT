5ca2ec14f11a8ce231473ea9e25651f0ca348258
darealsoapbox <nicolas@wpandco.fr>
2018-11-22 15:41:40 +0100

    formulaire de contact

--- contact.php
+++ contact.php
@@ -4,6 +4,27 @@
     <div class="bandeau">
         <h1>Contact</h1>
     </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="formulaire-contact">
+                <form>
+                    <p>
+                        <label>Votre nom</label>
+                    </p>
+                    <p>
+                        <label>Votre email</label>
+                    </p>
+                    <p>
+                        <label>Objet</label>
+                    </p>
+                    <p>
+                        <label>Corps du message</label>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
 </main>
 
 <?php include('footer.php'); ?>
