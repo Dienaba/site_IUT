87fa57f2ca1aa2a7e173b18a4b0961a7e7235b6d
darealsoapbox <nicolas@wpandco.fr>
2018-11-22 16:40:27 +0100

    slider

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     <main class="container">
         <!-- Pour modifier les carousel mettre le modif du width ici -->
     
-        <div class="row container-banner">
+        <div class="container-banner">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
                 <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
