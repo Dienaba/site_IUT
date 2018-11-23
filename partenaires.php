69392585dc50c9ac22ee39f8691ae75ac2bdaede
Camara Dienaba <34746425+Dienaba@users.noreply.github.com>
2018-11-23 10:53:06 +0100

    modif partenaire

--- partenaires.php
+++ partenaires.php
@@ -9,37 +9,37 @@
         <div class='containerPartenaire'>
             <img class='logoPartenaire' src="image/Atos.png" alt="">
             <div class="hover">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus voluptate magni obcaecati quibusdam ab saepe tenetur sint labore ipsam, mollitia corporis, accusantium dolor hic adipisci nam consectetur corrupti totam consequuntur.</p>
+                <p>Lorem ipsum </p>
             </div>
         </div>   
         <div class='containerPartenaire'>
             <img class='logoPartenaire' src="image/Capgemini.png" alt="">
             <div class="hover">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus voluptate magni obcaecati quibusdam ab saepe tenetur sint labore ipsam, mollitia corporis, accusantium dolor hic adipisci nam consectetur corrupti totam consequuntur.</p>
+                <p>Lorem ipsum</p>
             </div>
         </div>  
         <div class='containerPartenaire'>
             <img class='logoPartenaire' src="image/Cigref.png" alt="">
             <div class="hover">
-                <p class="text"> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus voluptate magni obcaecati quibusdam ab saepe tenetur sint labore ipsam, mollitia corporis, accusantium dolor hic adipisci nam consectetur corrupti totam consequuntur.</p>
+                <p class="text"> Lorem ipsum</p>
             </div>
         </div>
         <div class='containerPartenaire'>
             <img class='logoPartenaire' src="image/IBM.png" alt="">
             <div class="hover">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus voluptate magni obcaecati quibusdam ab saepe tenetur sint labore ipsam, mollitia corporis, accusantium dolor hic adipisci nam consectetur corrupti totam consequuntur.</p>
+                <p>Lorem ipsum</p>
             </div>
         </div> 
         <div class='containerPartenaire'>
             <img class='logoPartenaire' src="image/microsoft.png" alt="">
             <div class="hover">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus voluptate magni obcaecati quibusdam ab saepe tenetur sint labore ipsam, mollitia corporis, accusantium dolor hic adipisci nam consectetur corrupti totam consequuntur.</p>
-            </div>
-        </div> 
+                <p>Lorem ipsum </p>
+                 </div>
+            </div> 
         <div class='containerPartenaire'>
              <img class='logoPartenaire' src="image/unb.png" alt="">
             <div class="hover">
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus voluptate magni obcaecati quibusdam ab saepe tenetur sint labore ipsam, mollitia corporis, accusantium dolor hic adipisci nam consectetur corrupti totam consequuntur.</p>
+                <p>Lorem ipsum</p>
             </div>
         </div> 
     </div>
