5042e6a6667ac84c23de2aabdaac14054c8d0fb2
darealsoapbox <nicolas@wpandco.fr>
2018-12-20 10:09:52 +0100

    search

--- search.php
+++ search.php
@@ -25,3 +25,11 @@
         }
     }
 ?>
+
+<?php
+    while($terme_trouve = $select_terme->fetch()) {
+        echo "<div><h2>".$terme_trouve['titre']."</h2><p> ".$terme_trouve['contenu']."</p>";
+    }
+
+    $select_terme->closeCursor();
+?>
