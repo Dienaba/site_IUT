be6f70483c9d2fc07bfff948ea4730fe1e4496b6
Camara Dienaba <34746425+Dienaba@users.noreply.github.com>
2018-12-20 11:14:53 +0100

    modif partie 'à propos'

--- apropos.php
+++ apropos.php
@@ -4,6 +4,15 @@
     <div class="bandeau">
         <h1>A propos</h1>
     </div>
+    <div class=mainAPropos>
+        <p>Ce portail décrit les formations proposées dans les départements d'informatique des IUT, DUT et licence professionnelle.</p>
+
+        <p>Les départements d'informatique des IUT de France forment des étudiants aux métiers de l'informatique en lien avec le domaine du développement de logiciels.
+        </br></br> Ils proposent ainsi une formation à taille humaine, encadrée par des professeurs disponibles, ainsi que par des professionnels expérimentés qui apportent aux étudiants une expérience pratique et concrète des métiers de l'informatique.</p>
+
+        <p>L'enseignement de l'informatique à l'IUT fait une large part à la dimension professionnelle tout en conservant des composantes fondamentales; l'étudiant acquiert ainsi des savoir-faires mais également des connaissances, des concepts de base et des méthodes de travail qu'il sera à même de mettre à profit en situation professionnelle.</p>
+        <img class="imagePropos" src="image/etudiantes.jpg" alt="Problème de connexion">
+    </div>
 </main>
 
 <?php include('footer.php'); ?>
