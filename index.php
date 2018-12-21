361865c73df1b03dd0416020f3b8a2d7a6e32171
darealsoapbox <nicolas@wpandco.fr>
2018-12-21 11:42:55 +0100

    offres d'emploi

--- index.php
+++ index.php
@@ -112,8 +112,8 @@
                             <hr>
                             <li>Offre n°6</li>
                         <ul>
+                        <div class="show-more">Voir d'autres offres</div>
                     </div>
-                    <div class="show-more">Voir d'autres offres</div>
                 </div>
                 <div class="col-xs-12 col-sm-6 col-md-4">
                     <div class="article-du-mois">
